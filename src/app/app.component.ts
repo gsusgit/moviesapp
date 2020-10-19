40b22e6a23c05d3259a5f47ffa3e3b3d462bff76
Jesus Fernandez <fernandezjodar.jesus@gmail.com>
2020-10-19 16:42:34 +0200

    Creados componentes, servicios y modulos y probada la peticion http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { PeliculassService } from './services/peliculass.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
+  constructor(private peliculasService: PeliculassService) {
+    this.peliculasService.getCartelera()
+      .subscribe(respuesta => {
+        console.log(respuesta.results);
+      });
+  }
   title = 'moviesapp';
 }
---
+++ src/app/services/peliculass.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CarteleraResponse } from '../interfaces/cartelera-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeliculassService {
+
+  private URL = 'https://api.themoviedb.org/3/movie/';
+  private API_KEY = 'ea2ea434b3f0a962e8e6ded798bd1942';
+  constructor(private http: HttpClient) { }
+  getCartelera(): Observable<CarteleraResponse> {
+    return this.http.get<CarteleraResponse>(`https://api.themoviedb.org/3/movie/now_playing?api_key=ea2ea434b3f0a962e8e6ded798bd1942&language=es-ES&page=1`);
+  }
+}
