0e58dec1c945acedb4e2f8e6108f7f9124bd442d
Jesus Fernandez <fernandezjodar.jesus@gmail.com>
2021-12-23 10:31:06 +0100

    Fix 404 on refresh page (Production Build)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { PagesModule } from './pages/pages.module';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,7 @@ import { PagesModule } from './pages/pages.module';
     ComponentsModule,
     PagesModule
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
